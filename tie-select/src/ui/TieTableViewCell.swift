2d5e6cb293b76bf97509277d32b11064dc18ca3e
kohsaka <>
2023-10-05 18:44:27 +0900

    fixed

--- tie-select/src/ui/TieTableViewCell.swift
+++ tie-select/src/ui/TieTableViewCell.swift
@@ -15,16 +15,5 @@ class TieTableViewCell: UITableViewCell {
     @IBOutlet weak var name: UILabel!
     // イメージ
     @IBOutlet weak var img: UIImageView!
-    
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
 
 }
